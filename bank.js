075282173d2df227691215995384f693dc544024
David Rawson <dm.rawson@gmail.com>
2018-02-14 15:25:54 +0000

    Initial commit

---
+++ bank.js
@@ -0,0 +1,54 @@
+var Bank = function() {
+  this.accounts = [];
+}
+
+Bank.prototype.addAccount = function (account) {
+  this.accounts.push(account);
+}
+
+Bank.prototype.accountByName = function (name){
+  const account = this.accounts.find(function(account){
+    return account.name === name;
+  });
+  return account;
+}
+
+Bank.prototype.largestAccount = function(){
+  let tempAccount = this.accounts[0];
+
+  this.accounts.forEach(function(account){
+    if (account.value > tempAccount.value){
+      tempAccount = account;
+    }
+  })
+  return tempAccount
+}
+
+Bank.prototype.payInterest = function(){
+  const mappedArray = this.accounts.map(function(account){
+    account.value *= 1.1;
+    return account;
+  })
+  this.accounts = mappedArray;
+}
+
+Bank.prototype.businessAccounts = function(){
+  const businessAccounts = this.accounts.filter(function(account){
+    return account.type === 'business';
+  })
+  return businessAccounts;
+}
+
+Bank.prototype.totalValue = function(){
+  const total = this.accounts.reduce(function(runningTotal, account){
+    return runningTotal + account.value;
+  }, 0)
+  return total;
+}
+
+Bank.prototype.averageValue = function(){
+  const average = this.totalValue()/this.accounts.length;
+  return average;
+}
+
+module.exports = Bank;
